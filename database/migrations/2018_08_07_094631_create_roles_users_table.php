6d7e650ac613e3ee30f2bdf58ef7c6ee3af0bf86
Mahmoud-Abdelfatah <Mahmoud.Abdelfata@outlook.com>
2020-07-29 17:50:04 +0200

    first commit

---
+++ database/migrations/2018_08_07_094631_create_roles_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRolesUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('role_user', function (Blueprint $table) {
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->reference('id')->on('users');
+            $table->unsignedInteger('role_id');
+            $table->foreign('role_id')->reference('id')->on('roles');
+        
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles_users');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+
+              <div class="content coustm">
+                <div class="coustm1">
+                    <div class="title m-b-md">
+                        <b>Online Exam</b>
+                    </div>
+
+                    <div class="links">
+                        <a href="" style="background-color: #ff9f3b;padding: 10px;margin: 10px;color: black;border-radius: 5px;">Create Exam</a>
+                        <a href="" style="background-color: #ff9f3b;padding: 10px;margin: 10px;color: black;border-radius: 5px;">Give Exam</a>
+                        <a href="" style="background-color: #ff9f3b;padding: 10px;margin: 10px;color: black;border-radius: 5px;">Result</a>
+                    
+                    </div>
+                </div>
+            </div>
+        </div>
+
+@endsection
